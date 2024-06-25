cde37e3f69756330d52f00d4adc80bf79bc3a363
LViogeat <louis.viogeat@gmail.com>
2024-06-25 09:23:50 +0200

    design

--- src/modules/govetia/components/Projects.jsx
+++ src/modules/govetia/components/Projects.jsx
@@ -1,7 +1,7 @@
 //import logo from '../assets/logo.png'
 import { Box, Grid, Flex } from '@chakra-ui/react';
 import '../styles/Projects.css';
-import { Image, Text } from "@chakra-ui/react";
+import { Image, Text, Heading } from "@chakra-ui/react";
 import React from 'react';
 import DeviceCard from './DeviceCard';
 
@@ -25,23 +25,23 @@ const projectsDetails = [
   //   picture: "/assets/tarawa.png",
   //   alt: 'Page accueil site web Tfek'
   // },
-  {
-    title: "Tarawa : Hôtel de luxe",
-    url: "/tarawa",
-    imacPicture: "/assets/tarawa.png",
-    macbookPicture: "/assets/tarawa.png",
-    iphonePicture: "/assets/tarawa.png",
-    macbookVideo: "/assets/videos/macbook-tarawa.mp4",
-    alt: 'Page accueil site web Tarawa'
-  },
-  {
-    title: "Oh my food : Restaurant",
-    url: "/ohMyFood",
-    imacPicture: "/assets/oh_my_food.png",
-    macbookPicture: "/assets/oh_my_food.png",
-    iphonePicture: "/assets/oh_my_food.png",
-    alt: 'Page accueil site web Oh my food'
-  }
+  // {
+  //   title: "Tarawa : Hôtel de luxe",
+  //   url: "/tarawa",
+  //   imacPicture: "/assets/tarawa.png",
+  //   macbookPicture: "/assets/tarawa.png",
+  //   iphonePicture: "/assets/tarawa.png",
+  //   macbookVideo: "/assets/videos/macbook-tarawa.mp4",
+  //   alt: 'Page accueil site web Tarawa'
+  // },
+  // {
+  //   title: "Oh my food : Restaurant",
+  //   url: "/ohMyFood",
+  //   imacPicture: "/assets/oh_my_food.png",
+  //   macbookPicture: "/assets/oh_my_food.png",
+  //   iphonePicture: "/assets/oh_my_food.png",
+  //   alt: 'Page accueil site web Oh my food'
+  // }
 ]
 
 
@@ -49,7 +49,13 @@ function Projects(props) {
   const [isLargerThan768] = useMediaQuery("(min-width: 900px)")
 
   return (
-    <Grid templateColumns={isLargerThan768 ? 'repeat(1, 1fr)' : 'repeat(1, 1fr)'} gap={0}>
+    <Grid style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      }} templateColumns={isLargerThan768 ? 'repeat(1, 1fr)' : 'repeat(1, 1fr)'} gap={0}>
+      <Heading pt={'2rem'} >Découvrez nos créations</Heading>
       {projectsDetails.map((project, index) => (
         <DeviceCard 
             key={`${project.title}-${index}`}
